1ff9a9ec9f0d36d52b8ca1b9b28dfe2657a75182
Andrew <andrewmoren93@gmail.com>
2024-03-06 07:04:47 +0300

    add log

--- js/startGame.js
+++ js/startGame.js
@@ -1,3 +1,6 @@
 export const startGame = (difficult) => {
   let firstCard = null;
+  let secondCard = null;
+  let clickable = true;
+  const gameSection = document.querySelector(".game-section__container");
 };
