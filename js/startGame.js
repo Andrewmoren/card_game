c06b88c5da32b2c225b5fc54b5bff985da3fa992
Andrew <andrewmoren93@gmail.com>
2024-03-11 23:27:42 +0300

    crteate Icons

--- js/startGame.js
+++ js/startGame.js
@@ -1,7 +1,12 @@
+import { createIconsArray, dublicateArray } from "./utils.js";
+
 export const startGame = (difficult) => {
   let firstCard = null;
   let secondCard = null;
   let clickable = true;
   const gameSection = document.querySelector(".game-section__container");
   const gameTable = document.createElement("div");
+  const cardsIcons = createIconsArray(difficult);
+  const dublicatedCardsIcons = dublicateArray(cardsIcons);
+  console.log(dublicatedCardsIcons);
 };
--- js/utils.js
+++ js/utils.js
@@ -1,3 +1,5 @@
+export const dublicateArray = (array) => array.reduce((res, current), []);
+
 export const createIconsArray = (initialCount) => {
   const cardsIcons = [
     "copass",
@@ -9,4 +11,15 @@ export const createIconsArray = (initialCount) => {
     "atom",
     "basketball-ball",
   ];
+
+  switch (initialCount) {
+    case 10:
+      return cardsIcons.slice(0, 5);
+    case 12:
+      return cardsIcons.slice(0, 6);
+    case 14:
+      return cardsIcons.slice(0, 7);
+    case 16:
+      return cardsIcons;
+  }
 };
